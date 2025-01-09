ea641cc63704742443a658aeef904a0c86148e4d
Mainfrezzer <35963833+Mainfrezzer@users.noreply.github.com>
2025-01-09 13:31:03 +0100

    added wake method

--- source/m-wakeonlan/usr/local/emhttp/plugins/m-wakeonlan/include/wake.php
+++ source/m-wakeonlan/usr/local/emhttp/plugins/m-wakeonlan/include/wake.php
@@ -1,6 +1,7 @@
 <?
 $ifname = escapeshellarg($_POST['ifname']);
+$ipaddress = escapeshellarg($_POST['ip']);
 $mac    = escapeshellarg($_POST['mac']);
-$cmd = "etherwake -i $ifname $mac";
+$cmd = "etherwake -i $ifname $mac && etherwake -b $mac $ipaddress";
 shell_exec($cmd);
 ?>
